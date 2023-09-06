275021312a74013ebcbe9951a131a6bc293bf01f
jersoncesar20 <131275174+jersoncesar20@users.noreply.github.com>
2023-09-06 12:26:13 -0500

    inicio

---
+++ login/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>plantilla_cesar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="../plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<bodys>
+ 
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center min-vh-100">
+                        <div class="w-100 d-block bg-white shadow-lg rounded my-5"> 
+                            <div class="row">
+                                <div class="col-lg-5 d-none d-lg-block rounded-left">
+                                 <img src="../imagenes/mesa de trabajo 1.png" width="460px" height="470px" alt="">
+                                </div>
+                                <div class="col-lg-7">
+                                    <div class="p-5">
+                                      <center>
+                                        <h1 class="h5 mb-1">¡BIENVENIDOS!</h1>
+                                        <h6>a la tienda</h6>
+                                        <p class="h4 mb-1"> <h1>Movil iPhone</h1>
+                                            
+                                        </p>
+                                        </center>
+                                        <form class="user">
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Ingrese su Email">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Ingrese su Password">
+                                            </div>
+                                            <a href="" class="btn btn-primary btn-block waves-effect waves-light"> Ingresar </a>
+
+                                      
+                                            
+                                        </form>
+
+                                        <div class="row mt-4">
+                                            <div class="col-12 text-center">
+                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Olvidaste tu Contraseña?</a></p>
+                                                
+                                            </div> <!-- end col -->
+                                        </div>
+                                        <!-- end row -->
+                                    </div> <!-- end .padding-5 -->
+                                </div> <!-- end col -->
+                            </div> <!-- end row -->
+                        </div> <!-- end .w-100 -->
+                    </div> <!-- end .d-flex -->
+                </div> <!-- end col-->
+            </div> <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <!-- jQuery  -->
+    <script src="../plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/waves.js"></script>
+    <script src="../plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="../plantilla/Admin/vertical/assets/js/theme.js"></script>
+
+</body>
+
+</html>
